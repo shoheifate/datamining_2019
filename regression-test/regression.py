d4219f6fb0e634ba3eef22763b3106f523821029
Tamashiro syohei <e175752@ie.u-ryukyu.ac.jp>
2019-11-05 16:39:43 +0900

    regression add

---
+++ regression-test/regression.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class LinearRegression:
+    """
+    >>> import regression
+    >>> model = regression.LinearRegression()
+    >>> model.x
+    >>> #nothing
+    """
+    x = None
+    theta = None
+    y = None
+
+    def fit(self,x,y):
+        pass
+    def predict(self,x):
+        pass
+    def score(self,x,y):
+        pass
