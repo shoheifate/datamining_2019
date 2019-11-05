96c0d5d4577a561f03d798735221879d5a72de83
Tamashiro syohei <e175752@ie.u-ryukyu.ac.jp>
2019-11-05 17:12:02 +0900

    regression ver2 add

--- regression-test/regression.py
+++ regression-test/regression.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 class LinearRegression:
     """
     >>> import regression
@@ -12,7 +11,20 @@ class LinearRegression:
     y = None
 
     def fit(self,x,y):
-        pass
+        """
+        >>> import regression
+        >>> import importlib
+        >>> import datasets
+        >>> X,Y = datasets.load_linear_example1()
+        >>> importlib.reload(regression)
+        <module 'regression' from '/Users/e175752/datamining/regression-test/regression.py'>
+        >>> model = regression.LinearRegression()
+        >>> model.fit(X,Y)
+        >>> print(model.theta)
+        [5.30412371 0.49484536]
+        """
+        temp = np.linalg.inv(np.dot(x.T,x))
+        self.theta = np.dot(np.dot(temp,x.T),y)
     def predict(self,x):
         pass
     def score(self,x,y):
