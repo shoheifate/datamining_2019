4bc7fdc7b1b91c140e8910dd0b4616fca6620524
Tamashiro syohei <e175752@ie.u-ryukyu.ac.jp>
2019-11-12 15:47:27 +0900

    change linear to nolinear_ex1 add nonlinear_ex2

--- regression-test/datasets.py
+++ regression-test/datasets.py
@@ -1,11 +1,20 @@
 import numpy as np
 
-def load_linear_example1():
+def load_nonlinear_example1():
     """
-    >>> X,Y = load_linear_example1()
+    >>> X,Y = load_nonlinear_example1()
     >>> print(X[0])
-    [1 4]
+    [1. 0.]
     """
-    X = np.array([[1,4],[1,8],[1,13],[1,17]])
-    Y = np.array([7,10,11,14])
+    X = np.array([[1,0.0],[1,2.0],[1,3.9],[1,4.0]])
+    Y = np.array([4.0,0.0,3.0,2.0])
     return X,Y
+
+def polynomia2_features(input):
+    poly2 = input[:,1:]**2
+    return np.c_[input,poly2]
+
+def polynomial3_features(input):
+    poly2 = input[:,1:] ** 2
+    poly3 = input[:,1:] ** 3
+    return np.c_[input,poly2,poly3]
