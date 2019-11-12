d65ff694c7a12615d5c535aa652b982c9276e226
Tamashiro syohei <e175752@ie.u-ryukyu.ac.jp>
2019-11-12 16:29:25 +0900

    def fit() add

--- regression-test/regression.py
+++ regression-test/regression.py
@@ -56,3 +56,27 @@ class LinearRegression:
         """
         error = self.predict(x)-y
         return (error**2).sum()
+
+class RidgeRegression(LinearRegression):
+    alpha = None
+
+    def __init__(self,alpha=0.1):
+        self.alpha = alpha
+
+    def fit(self,input,output):
+        """
+        >>> import importlib
+        >>> import regression
+        >>> import datasets
+        >>> X,Y = datasets.load_nonlinear_example1()
+        >>> ex_X = datasets.polynomial3_features(X)
+        >>> importlib.reload(regression)
+        <module 'regression' from '/Users/e175752/datamining/regression-test/regression.py'>
+        >>> model = regression.RidgeRegression()
+        >>> model.fit(ex_X, Y)
+        >>> model.theta
+        array([ 3.54259714, -1.24971967, -0.68925104,  0.23695052])
+        """
+        xTx = np.dot(input.T, input)
+        I = np.eye(len(xTx))
+        self.theta = np.dot(np.dot(np.linalg.inv(xTx + self.alpha * I),input.T), output)
